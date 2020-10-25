4871d81f9722540cb121f3490a10890716aa2966
degurii <sansam709@gmail.com>
2020-10-26 02:39:17 +0900

    몽구스 모델, 스키마 생성

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = 5000;
 
 const app = express();
 const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 app.use('/api/products', productRoutes);
 
