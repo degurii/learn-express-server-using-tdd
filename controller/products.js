6ae8417e884c044d9ccde64883c5f987ee4637a7
degurii <sansam709@gmail.com>
2020-10-26 00:27:06 +0900

    뼈대 코드 작성

---
+++ controller/products.js
@@ -0,0 +1,3 @@
+exports.hello = (req, res) => {
+  res.send('<h1>안녕하세용!!!</h1>');
+};
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const PORT = 5000;
 
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Wolrd');
-});
+const productRoutes = require('./routes');
+
+app.use('/api/products', productRoutes);
+
+app.use(express.json()); // body parser
 
 app.listen(PORT);
-console.log(`Running on port ${PORT}`
+console.log(`Running on port ${PORT}`);
